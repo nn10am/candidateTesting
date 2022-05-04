f488e4035b54edb9e53a875ece1d13b4fb97d653
Pham Nhat <nhat.pham>
2022-05-04 22:01:57 +0700

    Format code

--- FE/ungvien/src/App.js
+++ FE/ungvien/src/App.js
@@ -1,59 +1,55 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios';
+import React, { useState } from 'react';
+import { Route, Routes } from "react-router-dom";
+import Finish from './Components/Finish';
 import LoginForm from './Components/FormLogin';
 import Test from './Components/Test';
 
-import Finish from './Components/Finish';
-import { Routes,Route } from "react-router-dom";
 
 function App() {
 
-  const[error,setError] = useState("");
-  const[user,setUser] = useState({name:"",email:"",phone:"",maDethi:""});
-  
-  
-  const Login =(details,listMaDeA )=>{
-    if(details.name!==""&&details.email!==""&&details.phone!==""&&listMaDeA.includes(details.maDethi* 1)){
-    console.log("Logged in");
-    setUser({
-      name:details.name,
-      email: details.email,
-      phone:details.phone,
-      maDethi:details.maDethi
-    });
+  const [error, setError] = useState("");
+  const [user, setUser] = useState({ name: "", email: "", phone: "", maDethi: "" });
 
-     } else if(details.name===""||details.email===""||details.phone===""){
-      
+
+  const Login = (details, listMaDeA) => {
+    if (details.name !== "" && details.email !== "" && details.phone !== "" && listMaDeA.includes(details.maDethi * 1)) {
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email,
+        phone: details.phone,
+        maDethi: details.maDethi
+      });
+
+    } else if (details.name === "" || details.email === "" || details.phone === "") {
+
       setError("Bạn cần nhập đủ thông tin");
     }
-    else{
+    else {
       setError("Nhập mã đề chưa chính xác")
     }
   }
-  
- 
-    if(user.email ===""){
-    
-      return(
-        <div>
-        <LoginForm  Login={Login} error={error}/></div>
-      );
-    }
-    else{
-     
 
-  return (
-    <div>
-      
-    
-      <Routes>
-        <Route path="/" element={<Test name={user.name} email={user.email} phone={user.phone} madethi={user.maDethi}/>} />
-        <Route path="/finish" element={<Finish />} />
-         
+
+  if (user.email === "") {
+
+    return (
+      <div>
+        <LoginForm Login={Login} error={error} /></div>
+    );
+  }
+  else {
+
+
+    return (
+      <div>
+        <Routes>
+          <Route path="/" element={<Test name={user.name} email={user.email} phone={user.phone} madethi={user.maDethi} />} />
+          <Route path="/finish" element={<Finish />} />
         </Routes>
-    </div>
-  )
-}
+      </div>
+    )
+  }
 }
 
 export default App
--- FE/ungvien/src/Components/FormLogin.js
+++ FE/ungvien/src/Components/FormLogin.js
@@ -1,59 +1,57 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-import"./FormLogin.css"
+import "./FormLogin.css"
 
-function LoginForm({Login,error}) {
-    const [details,setDetails]=useState({name:"",email:"",phone:"",maDethi:""});
-    const [listMaDeA,setListMaDeA] =useState([]);
+function LoginForm({ Login, error }) {
+    const [details, setDetails] = useState({ name: "", email: "", phone: "", maDethi: "" });
+    const [listMaDeA, setListMaDeA] = useState([1]);
     const submitHandler = e => {
         e.preventDefault();
-        Login(details,listMaDeA);
-        
+        Login(details, listMaDeA);
+
     }
 
-    useEffect(() =>{ //1
-        axios.get(`http://localhost:3000/test/`).then((res) =>{
+    useEffect(() => { //1
+        axios.get(`http://localhost:3000/test/`).then((res) => {
 
-            for(let b=[], i=0;i<res.data.length;i++){
-                let a=res.data[i].made;
-                b=b.concat(a);
-                console.log("mã đề mới làaaaaa",b);
-                
+            for (let b = [], i = 0; i < res.data.length; i++) {
+                let a = res.data[i].made;
+                b = b.concat(a);
+                console.log("mã đề mới làaaaaa", b);
+
                 setListMaDeA(b);
-              }
-              
+            }
+
         });
 
-},[]);
-  return ( 
-      <div className='HomeLogin'>
-        <form onSubmit={submitHandler}>
-            <div className="form-inner">
-                <h2>Tham gia đánh giá năng lực </h2>
-                {(error !== "") ? ( <div className='error'>{error}</div>) : ""}
-                <div className='form-group'>
-                 <label htmlFor='name'>Nhập họ và  tên của bạn:</label>
-                    <input type="text" name="name" id="name"  onChange={ e => setDetails({...details, name:e.target.value})} value={details.name}/>
+    }, []);
+    return (
+        <div className='HomeLogin'>
+            <form onSubmit={submitHandler}>
+                <div className="form-inner">
+                    <h2>Tham gia đánh giá năng lực </h2>
+                    {(error !== "") ? (<div className='error'>{error}</div>) : ""}
+                    <div className='form-group'>
+                        <label htmlFor='name'>Nhập họ và  tên của bạn:</label>
+                        <input type="text" name="name" id="name" onChange={e => setDetails({ ...details, name: e.target.value })} value={details.name} />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='phone'> Nhập số điện thoại:</label>
+                        <input type="number" name="phone" id="phone" onChange={e => setDetails({ ...details, phone: e.target.value })} value={details.phone} />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='email'>Nhập email:</label>
+                        <input type="email" name="email" id="email" onChange={e => setDetails({ ...details, email: e.target.value })} value={details.email} />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='made'>Nhập mã để làm bài:</label>
+                        <input type="number" name="made" id="made" onChange={e => setDetails({ ...details, maDethi: e.target.value })} value={details.maDethi} />
+                    </div>
+                    <input type="submit" value="Bắt đầu làm bài" />
                 </div>
-                <div className='form-group'>
-                 <label htmlFor='phone'> Nhập số điện thoại:</label>
-                    <input type="number" name="phone" id="phone"  onChange={ e => setDetails({...details, phone:e.target.value})} value={details.phone}/>
-                </div>
-                <div className='form-group'>
-                 <label htmlFor='email'>Nhập email:</label>
-                    <input type="email" name="email" id="email"  onChange={ e => setDetails({...details, email:e.target.value})} value={details.email}/>
-                </div>
-                <div className='form-group'>
-                 <label htmlFor='made'>Nhập mã để làm bài:</label>
-                    <input type="number" name="made" id="made"  onChange={ e => setDetails({...details, maDethi:e.target.value})} value={details.maDethi}/>
-                </div>
-                
-                 
-                <input type="submit" value="Bắt đầu làm bài" />
-            </div>
-        </form>
-    </div>
-  )
+            </form>
+        </div>
+    )
 }
 
 export default LoginForm
